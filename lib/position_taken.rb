5c1aa3f45db87d701fd300a81b10cd12f5260b53
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-30 16:14:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " "
-    return false
-  elsif board[index] == " " 
-    return nil
-  end
+   
 end
