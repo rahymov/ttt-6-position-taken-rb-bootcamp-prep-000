efc4bd6a572c9fc60257fddf93775081f7d4693b
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-30 16:16:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,6 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index]== nil
     return false
   elsif board[index] == "X" && board[index] == "O" 
+    return 
  end
 end
